b8ebdcef5718465659d64df0b80123225d29de1e
vnarod07 <vnarod2007@gmail.com>
2026-04-07 11:40:06 +0900

    Create 7785.py

---
+++ 4.7/7785.py
@@ -0,0 +1,11 @@
+N = int(input())
+output = []
+for i in range(N):
+    a = input().split()
+    if a[1] == 'enter':
+        output.append(a[0])
+    else:
+        output.remove(a[0])
+
+for i in output:        
+    print(i)
