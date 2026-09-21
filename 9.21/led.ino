2b394466f7d1a96b0ee4fecd9f3e682e5185cf66
vnarod07 <vnarod2007@gmail.com>
2026-09-21 12:19:07 +0900

    Create led.ino

---
+++ 9.21/led.ino
@@ -0,0 +1,35 @@
+
+int led;
+int period;
+int duty;
+int time;
+
+void setup() {
+  led = 9;
+  pinMode(led, OUTPUT);
+  set_period(100);
+  set_duty(255);
+ time = 0;
+
+}
+
+void loop() {
+ 
+  analogWrite(led, periodf(period,duty,time));
+  time = (time + 10)%period;
+  delay(10);
+}
+
+int periodf(int period, int duty, int x) {
+  int a = (int) (-abs(duty/(period*0.5f)*(x-period*0.5)) + duty);
+  if (a < 0) a = 0;
+  if (a > 255) a = 255;
+  return a; 
+  }
+
+ void set_period(int p) {
+  period = p;
+  }
+
+ void set_duty(int d) {
+  duty = d;}
